3d22f427c06f6fcffc1200fccf1873c7901f5f40
Alexander <kir.deal@gmail.com>
2017-03-09 00:52:58 +0300

    Initial Commit

---
+++ HiThere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HiThere
+//
+//  Created by Александр Кириченко on 09.03.17.
+//  Copyright © 2017 Александр Кириченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
